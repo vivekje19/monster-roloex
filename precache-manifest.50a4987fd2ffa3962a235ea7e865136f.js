d8f8c4d06b13ced5ae29527fc61f0e3776f25a62
Vivek Ranjan Pandey <vivekranjan.varanasi@gmail.com>
2021-11-02 23:30:46 +0530

    Updates

---
+++ precache-manifest.50a4987fd2ffa3962a235ea7e865136f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28436af03d41af2bffd6459b525a0ed5",
+    "url": "/monster-roloex/index.html"
+  },
+  {
+    "revision": "afdf15851f5236e39e7c",
+    "url": "/monster-roloex/static/css/main.93ce90ad.chunk.css"
+  },
+  {
+    "revision": "1ec637146c263ef8994d",
+    "url": "/monster-roloex/static/js/2.8d50370a.chunk.js"
+  },
+  {
+    "revision": "afdf15851f5236e39e7c",
+    "url": "/monster-roloex/static/js/main.6c30c6e4.chunk.js"
+  },
+  {
+    "revision": "2d150820e782d4c7b2de",
+    "url": "/monster-roloex/static/js/runtime~main.1d41efbb.js"
+  }
+]);
